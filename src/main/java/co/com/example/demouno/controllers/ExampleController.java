1f896bb18b09e1fa7db5491958a8dd8ba37cdcac
Norbey Danilo <24207969+ndmc98@users.noreply.github.com>
2023-07-06 22:40:52 -0500

    all update

---
+++ src/main/java/co/com/example/demouno/controllers/ExampleController.java
@@ -0,0 +1,27 @@
+package co.com.example.demouno.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ExampleController
+ *
+ * @author norbeydanilo
+ *
+ */
+@RestController
+@RequestMapping("/api/example")
+public class ExampleController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> getRoot() {
+        return ResponseEntity.ok("Hello World Root!");
+    }
+
+    @GetMapping("/hello-world")
+    public ResponseEntity<String> get() {
+        return ResponseEntity.ok("Hello World 2!");
+    }
+}
